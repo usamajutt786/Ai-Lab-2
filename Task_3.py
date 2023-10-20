d1cb783faf42c54dcc0b52a81030198c618eeeca
Usama <2021se41@student.uet.edu.pk>
2023-10-20 09:24:21 +0500

    Task_3

--- Task_3.py
+++ Task_3.py
@@ -1,8 +1,4 @@
 
-
-num1=int(input("Enter First number: "))
-num2=int(input("Enter Second number: "))
-
 def addition(a,b):
     return a+b
 def substract(a,b):
@@ -12,10 +8,24 @@ def divide(a,b):
 def multipy(a,b):
     return a*b
 def ShowMenu():
-    print("1.Addition ")
+    print("1.Add")
     print("2.Subtract ")
     print("3.Divide")
     print("4.Multiply")
-while 
+
+num1=int(input("Enter First number: "))
+num2=int(input("Enter Second number: "))
+ShowMenu()
+choice=int(input("Please Enter Your choice: "))
+if choice==1:
+    print(addition(num1,num2))
+elif choice==2:
+    print(substract(num1,num2))
+elif choice==3:
+    print(float(divide(num1,num2)))
+elif choice==4:
+    print(multipy(num1,num2))
+else:
+    print("Invalid Choice....")
     
 
